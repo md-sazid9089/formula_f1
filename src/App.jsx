74d899388345b0cc9c8e46c72d04444632e3a38a
syed mohammed sazid ullah <mdsazid9089@gmail.com>
2025-05-25 15:34:26 +0600

    just completed gsap animation ,was debugging for the error last 30 minutes.just a capital letter can cause a lot

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React, { useEffect } from 'react'
+import { gsap } from 'gsap'
+import { useGSAP } from "@gsap/react";
+export const App = () => {
+  useGSAP(() => {
+  const t2= gsap.timeline()
+  t2.to('.vi-mask-group', {
+    rotate: 10,
+    duration: 2,
+    ease: 'power4.easeInOut',
+    transformOrigin: '50% 50%',
+  })
+    .to('.vi-mask-group', {
+      scale: 10,
+      duration: 2,
+      delay: -1.8,
+      ease: 'expo.easeInOut',
+      transformOrigin: '50% 50%',
+      opacity: 0,
+    })
+  },[]);
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]'>
+        <svg viewBox='0 0 800 600' className='w-full h-full' preserveAspectRatio='xMidYMid slice'>
+          <defs>
+            <mask id='viMask'>
+              <rect width='100%' height='100%' fill='black' />
+              <g className='vi-mask-group'>
+                <text
+                  x='50%'
+                  y='50%'
+                  fontSize='250'
+                  textAnchor='middle'
+                  fill='white'
+                  dominantBaseline='middle'
+                  fontFamily='Arial Black'
+                >
+                  44
+                </text>
+              </g>
+            </mask>
+          </defs>
+          <rect width='800' height='600' fill='white' mask='url(#viMask)' />
+        </svg>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
